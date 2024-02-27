0b5ffcedf64e83155cb37d88074245834f579be6
PesPatron <temich2288@mail.ru>
2024-02-27 18:26:30 +0500

    Damage
    Damage

---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public FireballScript fireball;
+    public Transform armTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireball, armTransform.position, armTransform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/FireballScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+    public float Speed;
+    public float lifetime;
+    public float damage = 20;
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<MobHP>();
+        if(enemyHealth != null)
+        {
+            enemyHealth.mobHP -= damage;
+        }
+    }
+    private void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+    private void GoForward()
+    {
+        transform.position += transform.forward * Speed * Time.fixedDeltaTime;
+    }
+
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Invoke("DestroyGameObject", lifetime);
+    }
+
+    void FixedUpdate()
+    {
+        GoForward();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyGameObject();
+    }
+}
