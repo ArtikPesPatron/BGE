40c2c5538482fd9369d1c886ea51d68c0d37f4d2
PesPatron <temich2288@mail.ru>
2024-03-21 12:22:35 +0500

    Big Update 0.1
    Changelog:
    UI:
    1. Added Gameplay UI:
    · Added Healthbar
    · Added Crosshair
    2. Added Game Over Screen
    Gameplay:
    1. Added Mob Attack
    2. Added Player Health
    Bugfix

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float HP = 100;
+    private float maxHP;
+    public RectTransform valueRectTransform;
+
+    public GameObject gameplayUI;
+    public GameObject gameOverScreen;
+
+    private void DrawHealthBar()
+    {
+        valueRectTransform.anchorMax = new Vector2(HP / maxHP, 1);
+    }
+
+    private void PlayerDeath()
+    {
+        gameplayUI.SetActive(false);
+        gameOverScreen.SetActive(true);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<CameraRotation>().enabled = false;
+        GetComponent<FireballCaster>().enabled = false;
+    }
+    public void DealDamage(float damage)
+    {
+        HP -= damage;
+        if (HP <= 0)
+        {
+            PlayerDeath();
+        }
+
+        DrawHealthBar();
+    }
+    void Start()
+    {
+        maxHP = HP;
+        DrawHealthBar();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
